f4b4c5f4e3f20be5cf96276e8154a637def3fc53
tukhtarov <tukhtarov@gmail.com>
2019-01-26 21:14:57 +0700

    Change database to MLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const URI = process.env.MONGODB_URI || 'mongodb://admin:Sm14puk7!@cluster0-shard-00-00-d7old.mongodb.net:27017,cluster0-shard-00-01-d7old.mongodb.net:27017,cluster0-shard-00-02-d7old.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+const URI = process.env.MONGODB_URI;
 mongoose.connect(URI, {useNewUrlParser: true});
 
 module.exports = {
