c01d2f60cb258ee84e319d9a6a948a5eb242b978
tukhtarov <tukhtarov@gmail.com>
2019-01-26 21:44:57 +0700

    Add MONGODB_URI heroke variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const URI = process.env.MONGODB_URI;
+const URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.connect(URI, {useNewUrlParser: true});
 
 module.exports = {
