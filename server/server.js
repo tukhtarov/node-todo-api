78ebc2dd34942abaa5d88fffcf3f5e8c8a33d517
tukhtarov <tukhtarov@gmail.com>
2019-01-27 15:36:08 +0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -39,6 +39,23 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req,res) => {
+    const _id = req.params.id;
+    if (!ObjectID.isValid(_id)) {
+        return res.status(404).send();
+    }
+    Todo.findOneAndDelete({_id}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        })
+    }, e => {
+        res.status(400).send();
+    })
+});
+
 app.post('/todos', (req, res) => {
     const todo = new Todo({
         text: req.body.text
