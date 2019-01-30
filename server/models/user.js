38703f6b4c5e2420992c4818c6e6c599cb5135d8
tukhtarov <tukhtarov@gmail.com>
2019-01-30 15:20:44 +0700

    Add /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -38,7 +38,7 @@ UserSchema.methods.toJSON = function () {
     return _.pick(userObject, ['_id', 'email']);
 }
 
-UserSchema.methods.generateAuthToken = async function () {
+UserSchema.methods.generateAuthToken = function () {
     const user = this;
     const access = 'auth';
     const token = jwt.sign({
@@ -48,6 +48,21 @@ UserSchema.methods.generateAuthToken = async function () {
     return user.save().then(() => {
         return token;
     });
+}
+
+UserSchema.statics.findByToken = function (token) {
+    const User = this;
+    let decoded;
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    })
 }
 
 const User = mongoose.model('User', UserSchema);
